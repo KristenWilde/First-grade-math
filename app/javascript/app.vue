6f010b77178af4739c828aafc09a0b49d12cf093
Kristen Wilde <kristen.wilde@gmail.com>
2018-05-12 23:05:17 -0400

    display problem list with styles

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="app">
-    <p>Hello {{ username }}</p>
-    <p>Here are your problems.</p>
-    <p>{{ problems }}</p>
+    <h2>Hello {{ username }}!</h2>
+    <p id="info-problems">You’ll be working on the yellow problems below. For an extra challenge you can click on more problems.</p>
+    <p id="info-mastered-problems">When you "master" a problem it will be pink. We'll review these problems too.</p>
+    <button>Start</button>
+    <ol id="problem-list">
+      <li v-for="problem in problems" class="problem">
+        {{ problem.minuend }} - {{ problem.subtrahend }}
+      </li>
+    </ol>
   </div>
 </template>
 
@@ -10,6 +16,8 @@
 export default {
   props: ["username", "problems"],
 }
+
+
 </script>
 
 <style scoped>
