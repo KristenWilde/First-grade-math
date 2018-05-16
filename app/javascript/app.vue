d0dfe54b72af7d2159a017c7b63d7c08e6b41c3b
Kristen Wilde <kristen.wilde@gmail.com>
2018-05-16 13:00:41 -0400

    refactor - li's not components

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -3,27 +3,27 @@
     <div id="app-index">
       <h2>Hello {{ username }}!</h2>
       <p id="info-problems">You’ll be working on the yellow problems below. For an extra challenge you can click on more problems.</p>
-      <p id="info-mastered-problems">When you "master" a problem it will be pink.</p>
+      <p id="info-mastered-problems">When you answer a problem two times in less than 5 seconds, it will be pink.</p>
       <button>Start</button>
       <ol id="problem-list">
-        <problem-li v-for="problem in problems"
-                    v-bind:minuend="problem.minuend"
-                    v-bind:subtrahend="problem.subtrahend"
-                    v-bind:success_times="problem.success_times"
-                    v-bind:key="problem.id"
-                    v-bind:problem_id="problem.id"
-                    v-bind:data-working="workingIds">
-         </problem-li>
+        <li v-for="(problem, idx) in problems"
+            class="problem"
+            v-bind:class="{
+                mastered: isMastered(problem),
+                working: isSelected(problem),
+              }"
+            v-on:click="toggleProblem(problem)">
+            {{ problem.minuend }} - {{ problem.subtrahend }}
+         </li>
       </ol>
     </div>
     <div id="practice">
-      <problem-card v-for="problem in problems"
+      <problem-card v-for="problem in workingProblems"
                     v-bind:minuend="problem.minuend"
                     v-bind:subtrahend="problem.subtrahend"
                     v-bind:success_times="problem.success_times"
                     v-bind:key="problem.id"
                     v-bind:problem_id="problem.id"
-                    v-bind:data-working="workingIds"
                     v-on:success="incrementSuccess($event)">
       </problem-card>
     </div>
@@ -31,38 +31,53 @@
 </template>
 
 <script>
-import Li from 'li.vue'
 import Card from 'card.vue'
 
 export default {
   name: 'app',
   props: ["username", "problems"],
-
-  computed: {
-    workingIds: function() {
-      const ids = [];
+  data() {
+    return { workingProblems: this.next8() }
+  },
+  methods: {
+    next8: function() {
+      const result = [];
       let idx = 0;
-      let currentId;
 
-      while (ids.length < 8) {
-        currentId = this.problems[idx].id;
+      while (result.length < 8) {
         if (this.problems[idx].success_times < 2) {
-          ids.push(currentId);
+          result.push(this.problems[idx]);
         }
         idx += 1;
       }
-      return ids;
+      return result;
+    },
+    toggleProblem(problem) {
+      if (this.workingProblems.includes(problem)){
+        this.remove(problem);
+      }
+      else {
+        this.workingProblems.push(problem)
+      }
+    },
+    remove(problem) {
+      const idx = this.workingProblems.indexOf(problem);
+      this.workingProblems.splice(idx, 1);
+    },
+    isSelected(problem) {
+      return this.workingProblems.includes(problem);
+    },
+    isMastered(problem) {
+      return problem.success_times >= 2;
+    },
+    find(problemId) {
+      return this.problems.filter((prob) => prob.id == problemId)[0];
+    },
+    incrementSuccess(problemId) {
+      this.find(problemId).success_times += 1;
     },
   },
-  methods: {
-    incrementSuccess: function(problemId) {
-      const problem = this.problems.filter((prob) => prob.id == problemId)[0];
-      console.log(problem.id)
-      problem.success_times += 1;
-    }
-  },
   components: {
-    'problem-li': Li,
     'problem-card': Card,
   }
 
