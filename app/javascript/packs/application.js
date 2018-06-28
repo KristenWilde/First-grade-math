21ffd3c9b350183c829980aaa55261589b729aba
Kristen Wilde <kristen.wilde@gmail.com>
2018-06-28 13:32:57 -0400

    remove unnecessary files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Kristen')
-
 import TurbolinksAdapter from 'vue-turbolinks';
 import Vue from 'vue/dist/vue.esm';
 import VueRouter from 'vue-router';
@@ -39,7 +37,7 @@ const router = new VueRouter({
   mode: 'history',
 });
 
-// Boilerplate for possible future refactor
+// for possible future refactor
 const store = new Vuex.Store({
   state: {
   },
